e38cd11e76ffa1a842f7590c1d2ce2587981fdce
Donal Cahill <din@ury.york.ac.uk>
2013-02-04 11:47:07 +0000

    Change to 4 space indent LIGAF.

--- lemon.php
+++ lemon.php
@@ -5,69 +5,71 @@ $logs_path = "/mnt/logs/";
 
 $url = 'http://urybsod.york.ac.uk:9090/'.$_GET['action'];
 $options = array('user' => $_SESSION['memberid'],
-				 'start' => $_GET['start'],
-				 'end' => $_GET['end'],
-				 'format' => $_GET['format'],
-				 'title' => $_GET['title']);
+                 'start' => $_GET['start'],
+                 'end' => $_GET['end'],
+                 'format' => $_GET['format'],
+                 'title' => $_GET['title']);
 $r = new HttpRequest($url, HttpRequest::METH_GET);
 $r->setQueryData($options);
 
 switch ($_GET['action']) {
 case "make":
 case "progress":
-  try  {
+    try  {
 	$r->send();
 	echo $r->getResponseBody();
-  } catch (HttpException $ex) {
-  echo $ex;
+    } catch (HttpException $ex) {
+    echo $ex;
 }
 break;
 case "getrequests":
-  try {
+    try {
 	$r->send();
 	switch($r->getResponseCode()) {
 	case '200':
-	  echo $r->getResponseBody();
-	  break;
+            echo $r->getResponseBody();
+            break;
 	case '400':
-	  header('HTTP/1.1 400 Bad Request');
-	  echo $r->getResponseBody();
-	  break;
+            header('HTTP/1.1 400 Bad Request');
+            echo $r->getResponseBody();
+            break;
 	default:
-	  header('HTTP/1.1 500 Internal Server Error');
-	  echo $r->getResponseBody();
-	  break;
+            header('HTTP/1.1 500 Internal Server Error');
+            echo $r->getResponseBody();
+            break;
 	}
-  } catch (HttpException $ex) {
+    } catch (HttpException $ex) {
 	echo $ex;
-  }
-  break;
+    }
+    break;
 case "download":
-  try {
+    try {
 	$r->send();
 	switch ($r->getResponseCode()) {
 	case '200':
-	  $response = json_decode($r->getResponseBody());
-	  $file = $logs_path.$response->{"filename_disk"};
-	  $filename_user = $response->{"filename_user"};
-	  header("X-Sendfile: $file");
-	  header("Content-Type: application/octet-stream");
-	  header("Content-Disposition: attachment; filename=\"$filename_user\"");
-	  exit;
+            $response = json_decode($r->getResponseBody());
+            $file = $logs_path.$response->{"filename_disk"};
+            $filename_user = $response->{"filename_user"};
+            header("X-Sendfile: $file");
+            header("Content-Type: application/octet-stream");
+            header("Content-Disposition: attachment; filename=\"$filename_user\"");
+            exit;
 	case '403':
-	  // The log isn't ready yet. Say something about it?
-	  break;
+            // The log isn't ready yet. Say something about it?
+            break;
 	default:
-	  header('HTTP/1.1 500 Internal Server Error');
-	  echo $r->getResponseBody();
-	  break;
+            header('HTTP/1.1 500 Internal Server Error');
+            echo $r->getResponseBody();
+            break;
 	}
-  } catch (HttpException $ex) {
+    } catch (HttpException $ex) {
 	echo $ex;
-  }
-  break;
+    }
+    break;
+case "remove":
+    
 default:
-  echo "Error: unknown action.";
-  exit(1);
+    echo "Error: unknown action.";
+exit(1);
 }
 ?>
